69109a9a36e3be308018a03f3a37479d1d450092
Dmitry Halavach <kotik1@protonmail.com>
2024-04-30 23:07:19 +0300

    feat: hello world

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,60 @@
 org 0x7C00
 bits 16
 
+
+%define ENDL 0x0D, 0x0A
+
+
+start:
+    jmp main
+
+
+
+puts:
+    
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb               
+    or al, al          
+    jz .done
+
+    mov ah, 0x0E        
+    mov bh, 0          
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si    
+    ret
+    
+
 main:
-	hlt
-	
+    mov ax, 0           
+    mov ds, ax
+    mov es, ax
+    
+    ; stack
+    mov ss, ax
+    mov sp, 0x7C00     
+
+    mov si, msg_hello
+    call puts
+
+    hlt
+
 .halt:
-	jmp .halt
+    jmp .halt
+
+
+
+msg_hello: db 'Hello from the dumbest OS ever created!', ENDL, 0
+
 
 times 510-($-$$) db 0
 dw 0AA55h
